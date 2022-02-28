ae88d150d9c1195c6be969bb44833bc3b66ee27f
tony <yahooleo36@gmail.com>
2022-02-28 21:32:26 +0800

    了解異步的發生

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import Parent from './components/Parent';
+import { useState } from 'react';
 
 function App() {
-  return <Parent />;
+  const [count, setCount] = useState(0);
+  console.log('顯示狀態用的：', count);
+  return (
+    <>
+      <h1>{count}</h1>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+          // console.log('綁在監聽器中的', count);
+        }}
+      >
+        按一次次數加上1
+      </button>
+    </>
+  );
 }
 
 export default App;
