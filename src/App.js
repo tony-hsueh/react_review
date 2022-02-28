7cf6c4f8c0606a14f06bc73fc66f313299f7a38c
tony <yahooleo36@gmail.com>
2022-02-28 22:03:32 +0800

    表單元素的使用

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
 import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
-  console.log('顯示狀態用的：', count);
+  const [inputText, setInputText] = useState('');
+  const [textArea, setTextArea] = useState('');
+
+  const [radioValue, setRadioValue] = useState('');
+  const foodOptions = ['排骨飯', '拉麵', '雞腿飯'];
+  // 可在此陣列中插入新的食物種類
   return (
     <>
-      <h1>{count}</h1>
-      <button
-        onClick={() => {
-          setCount(count + 1);
-          // console.log('綁在監聽器中的', count);
+      <h1>可於react控制的表單元素</h1>
+      <h2>1.文字輸入框</h2>
+      <input
+        type="text"
+        value={inputText}
+        onChange={(e) => {
+          setInputText(e.target.value);
         }}
-      >
-        按一次次數加上1
-      </button>
+      />
+      <h2>2.文字輸入區域</h2>
+      <textarea
+        value={textArea}
+        onChange={(e) => {
+          setTextArea(e.target.value);
+        }}
+      />
+      <h2>3.選項按鈕</h2>
+      {foodOptions.map((v, i) => {
+        return (
+          <div key={i}>
+            <input
+              type="radio"
+              value={v}
+              id={'foodOption' + i}
+              checked={v === radioValue}
+              onChange={(e) => {
+                setRadioValue(e.target.value);
+              }}
+            />
+            <label htmlFor={'foodOption' + i}>{v}</label>
+          </div>
+        );
+      })}
     </>
   );
 }
