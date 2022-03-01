323ca6e72b8934b905a87a346baf9bde46a91210
tony <yahooleo36@gmail.com>
2022-03-01 08:17:44 +0800

    操作類別行元件的useEffect

--- src/App.js
+++ src/App.js
@@ -1,103 +1,18 @@
 import { useState } from 'react';
+import CCLifecycle from './components/CCLifecycle';
 
 function App() {
-  const [inputText, setInputText] = useState('');
-  const [textArea, setTextArea] = useState('');
-
-  const [radioValue, setRadioValue] = useState('');
-  const foodOptions = ['排骨飯', '拉麵', '雞腿飯'];
-  // 可在此陣列中插入新的食物種類
-  const [selectValue, setSelectValue] = useState('');
-
-  const [agree, setAgree] = useState(false);
-  const [likeList, setLikeList] = useState([]);
-  const fruitOptions = ['西瓜', '芒果', '芭樂'];
+  const [alive, setAlive] = useState(true);
   return (
     <>
-      <h1>可於react控制的表單元素</h1>
-      <h2>1.文字輸入框</h2>
-      <input
-        type="text"
-        value={inputText}
-        onChange={(e) => {
-          setInputText(e.target.value);
-        }}
-      />
-      <h2>2.文字輸入區域</h2>
-      <textarea
-        value={textArea}
-        onChange={(e) => {
-          setTextArea(e.target.value);
-        }}
-      />
-      <h2>3.選項按鈕</h2>
-      {foodOptions.map((v, i) => {
-        return (
-          <div key={i}>
-            <input
-              type="radio"
-              value={v}
-              id={'foodOption' + i}
-              checked={v === radioValue}
-              onChange={(e) => {
-                setRadioValue(e.target.value);
-              }}
-            />
-            <label htmlFor={'foodOption' + i}>{v}</label>
-          </div>
-        );
-      })}
-      <h2>4.下拉選單</h2>
-      <label htmlFor="cars">選擇車子的牌子：</label>
-      <select
-        name="cars"
-        id="cars"
-        value={selectValue}
-        onChange={(e) => {
-          setSelectValue(e.target.value);
+      {alive && <CCLifecycle />}
+      <button
+        onClick={() => {
+          setAlive(!alive);
         }}
       >
-        <option value="">請選擇</option>
-        <option value="volvo">volvo</option>
-        <option value="toyota">toyota</option>
-        <option value="audi">audi</option>
-        <option value="honda">honda</option>
-      </select>
-      <h2>核對方塊(單一)</h2>
-      <input
-        type="checkbox"
-        checked={agree}
-        onChange={(e) => {
-          setAgree(e.target.checked);
-        }}
-      />
-      <label>同意註冊條款</label>
-      <h2>核對方塊(群組)</h2>
-      <p>請選擇您喜歡的水果</p>
-      {fruitOptions.map((v, i) => {
-        return (
-          <div key={i}>
-            <input
-              type="checkbox"
-              value={v}
-              checked={likeList.includes(v)}
-              onChange={(e) => {
-                if (likeList.includes(e.target.value)) {
-                  const newLikeList = likeList.filter(
-                    (v) => v !== e.target.value
-                  );
-                  setLikeList(newLikeList);
-                } else {
-                  const newLikeList = [...likeList];
-                  newLikeList.push(e.target.value);
-                  setLikeList(newLikeList);
-                }
-              }}
-            />
-            <label>{v}</label>
-          </div>
-        );
-      })}
+        {alive ? '消失' : '出現'}
+      </button>
     </>
   );
 }
---
+++ src/components/CCLifecycle.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class CCLifecycle extends React.Component {
+  constructor() {
+    super();
+    this.state = { total: 0 };
+    console.log('constructor');
+  }
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+  componentDidUpdate() {
+    console.log('componentDidUpdate');
+  }
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  }
+  render() {
+    console.log('render');
+    return (
+      <>
+        <h1
+          onClick={() => {
+            this.setState({ total: this.state.total + 1 });
+          }}
+        >
+          {this.state.total}
+        </h1>
+      </>
+    );
+  }
+}
+export default CCLifecycle;
